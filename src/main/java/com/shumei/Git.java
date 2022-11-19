b6c0de0a24c8f1579eaf3862ff132552fce7736e
shumeiba <cyhlbh2010@gmail.com>
2022-11-19 10:14:16 +0800

    hot-fix commit2

--- src/main/java/com/shumei/Git.java
+++ src/main/java/com/shumei/Git.java
@@ -5,6 +5,6 @@ public class Git {
         System.out.println("hello,git");
         System.out.println("hello,github");
         System.out.println("hello,java");
-        System.out.println("hello,java4");
+        System.out.println("hello,hot-fix1");
     }
 }
